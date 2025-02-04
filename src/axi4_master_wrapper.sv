81d218ea6db5fc80d618baf8e9527135e12f28f7
MahdiGMK <mbahramiand@gmail.com>
2025-02-05 01:37:07 +0330

    + axi reset

--- src/axi4_master_wrapper.sv
+++ src/axi4_master_wrapper.sv
@@ -1,34 +1,39 @@
-module axi4_slave_wrapper #(
+module axi4_master_wrapper #(
     parameter int SZ  = 32,
     parameter int ASZ = 2,
     parameter int DSZ = 8
 ) (
     input _rst,
     input clk,
+    output out_clk,
+    input [SZ-1:0] a,
+    input [SZ-1:0] b,
+    output [2*SZ-1:0] res,
     // AW
-    input [ASZ-1:0] awaddr,
-    input awvalid,
-    output reg awready,
+    output reg [ASZ-1:0] awaddr,
+    output reg awvalid,
+    input awready,
     // W
-    input [DSZ-1:0] wdata,
-    input wvalid,
-    output reg wready,
-    input wlast,
+    output reg [DSZ-1:0] wdata,
+    output reg wvalid,
+    input wready,
+    output reg wlast,
     // B
-    output reg bresp,  // 1 => ok
-    output reg bvalid,
-    input bready,
+    input bresp,  // 1 => ok
+    input bvalid,
+    output reg bready,
     // AR
-    input [ASZ-1:0] araddr,
-    input arvalid,
-    output reg arready,
+    output reg [ASZ-1:0] araddr,
+    output reg arvalid,
+    input arready,
     // R
-    output reg [DSZ-1:0] rdata,
-    output reg rvalid,
-    input rready,
-    output reg rlast,
-    output reg rresp  // 1 => ok
+    input [DSZ-1:0] rdata,
+    input rvalid,
+    output reg rready,
+    input rlast,
+    input rresp  // 1 => ok
 );
+    assign out_clk = clk;
 
     reg [DSZ-1:0] inregs[(1<<ASZ) * (SZ/DSZ)];
     reg start;
@@ -36,55 +41,13 @@ module axi4_slave_wrapper #(
     wire [DSZ-1:0] outregs[2*SZ/DSZ];
     wire mulready;
 
-
-    mult #(
-        .SZ(SZ)
-    ) main_module (
-        .a({inregs[3], inregs[2], inregs[1], inregs[0]}),
-        .b({inregs[7], inregs[6], inregs[5], inregs[4]}),
-        .res({
-            outregs[7],
-            outregs[6],
-            outregs[5],
-            outregs[4],
-            outregs[3],
-            outregs[2],
-            outregs[1],
-            outregs[0]
-        }),
-        .start(start),
-        .ready(mulready),
-        ._rst(_rst),
-        .clk(clk)
-    );
-
     reg [ASZ-1:0] awaddr_reg;
     reg [ASZ-1:0] araddr_reg;
     reg [7:0] wpos;
     reg [7:0] rpos;
-
-    int i;
     always @(posedge clk, negedge _rst) begin
         if (~_rst) begin
             // zero all regs
-            wpos <= 0;
-            rpos <= 0;
-            awaddr_reg <= 0;
-            araddr_reg <= 0;
-            for (i = 0; i < (1 << ASZ) * (SZ / DSZ); i = i + 1) begin
-                inregs[i] <= 0;
-            end
-            start   <= 1;
-
-            wready  <= 0;
-            bresp   <= 0;
-            bvalid  <= 0;
-            rdata   <= 0;
-            rvalid  <= 0;
-            rlast   <= 0;
-            rresp   <= 0;
-
-
             awready <= 1;  // accept write req
             arready <= 1;  // accept read req
         end
