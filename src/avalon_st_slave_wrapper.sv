b672584cea52e7f6354cfbcbdf68ce82286e4507
MahdiGMK <mbahramiand@gmail.com>
2025-02-04 19:37:59 +0330

    now it begins

--- src/avalon_st_slave_wrapper.sv
+++ src/avalon_st_slave_wrapper.sv
@@ -1,2 +1,27 @@
-module avalon_st_wrapper ();
+module avalon_st_slave_wrapper #(
+    parameter int SZ = 32
+) (
+    input _rst,
+    input clk
+);
+
+    reg [SZ-1:0] areg;
+    reg [SZ-1:0] breg;
+    reg start;
+
+    wire [2*SZ-1:0] res;
+    wire ready;
+
+
+    mult #(
+        .SZ(SZ)
+    ) main_module (
+        .a(areg),
+        .b(breg),
+        .res(res),
+        .start(start),
+        .ready(ready),
+        ._rst(_rst),
+        .clk(clk)
+    );
 endmodule
--- src/axi4_stream_slave_wrapper.sv
+++ src/axi4_stream_slave_wrapper.sv
@@ -1,2 +1,27 @@
-module axi4_stream_wrapper ();
+module axi4_stream_slave_wrapper #(
+    parameter int SZ = 32
+) (
+    input _rst,
+    input clk
+);
+
+    reg [SZ-1:0] areg;
+    reg [SZ-1:0] breg;
+    reg start;
+
+    wire [2*SZ-1:0] res;
+    wire ready;
+
+
+    mult #(
+        .SZ(SZ)
+    ) main_module (
+        .a(areg),
+        .b(breg),
+        .res(res),
+        .start(start),
+        .ready(ready),
+        ._rst(_rst),
+        .clk(clk)
+    );
 endmodule
