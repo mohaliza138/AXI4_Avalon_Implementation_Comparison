9041459cee59c6c8acb378f82671a159163faa3f
MahdiGMK <mbahramiand@gmail.com>
2025-02-05 06:19:17 +0330

    axi stream ...

---
+++ src/axi4_stream_master_wrapper.sv
@@ -0,0 +1,126 @@
+module axi4_master_wrapper #(
+    parameter int SZ  = 32,
+    parameter int ASZ = 2,
+    parameter int DSZ = 8
+) (
+    input _rst,
+    input clk,
+    output out_clk,
+    input [SZ-1:0] a,
+    input [SZ-1:0] b,
+    output [2*SZ-1:0] res,
+    // AW
+    output reg [ASZ-1:0] awaddr,
+    output reg awvalid,
+    input awready,
+    // W
+    output reg [DSZ-1:0] wdata,
+    output reg wvalid,
+    input wready,
+    output reg wlast,
+    // B
+    input bresp,  // 1 => ok
+    input bvalid,
+    output reg bready,
+    // AR
+    output reg [ASZ-1:0] araddr,
+    output reg arvalid,
+    input arready,
+    // R
+    input [DSZ-1:0] rdata,
+    input rvalid,
+    output reg rready,
+    input rlast,
+    input rresp  // 1 => ok
+);
+    assign out_clk = clk;
+
+    wire [DSZ-1:0] outregs[(2) * (SZ / DSZ)];
+    assign outregs[0] = a[7:0];
+    assign outregs[1] = a[15:8];
+    assign outregs[2] = a[23:16];
+    assign outregs[3] = a[31:24];
+    assign outregs[4] = b[7:0];
+    assign outregs[5] = b[15:8];
+    assign outregs[6] = b[23:16];
+    assign outregs[7] = b[31:24];
+
+    reg [DSZ-1:0] inregs[2 * SZ / DSZ];
+    assign res = {
+        inregs[7], inregs[6], inregs[5], inregs[4], inregs[3], inregs[2], inregs[1], inregs[0]
+    };
+
+
+    reg [7:0] wpos;
+    reg [7:0] rpos;
+    int i;
+    always @(posedge clk, negedge _rst) begin
+        if (~_rst) begin
+            wdata  <= 0;
+            wvalid <= 0;
+            wlast  <= 0;
+            bready <= 0;
+            rready <= 0;
+            for (i = 0; i < 2 * SZ / DSZ; i = i + 1) begin
+                inregs[i] <= 0;
+            end
+            wpos <= 0;
+            rpos <= 0;
+            // setup data write
+            awaddr <= 0;
+            awvalid <= 1;
+            // setup result read
+            araddr <= 0;
+            arvalid <= 1;
+        end
+        else begin
+
+            if (awvalid & awready) begin
+                $display($time, " | master | ", "snd wdata : ", outregs[awaddr*4], " ind : %1d",
+                         awaddr * 4);
+                awvalid <= 0;
+                wdata   <= outregs[awaddr * 4];
+                wpos    <= awaddr * 4 + 1;
+                wvalid  <= 1;
+                wlast   <= 0;
+            end
+            if (wvalid & wready) begin
+                $display($time, " | master | ", "snd wdata : ", outregs[wpos], " ind : %1d", wpos);
+                wdata <= outregs[wpos];
+                wpos  <= wpos + 1;
+                wlast <= ((wpos & 3) == 3);
+            end
+            if (wvalid & wready & wlast) begin
+                wvalid <= 0;
+                wlast  <= 0;
+                wdata  <= 0;
+                bready <= 1;
+            end
+            if (bready & bvalid) begin
+                $display($time, " | master | ", "bresp : ", bresp);
+                bready  <= 0;
+                awvalid <= 1;
+                awaddr  <= (awaddr + 1) & 1;
+            end
+
+            if (arvalid & arready) begin
+                $display($time, " | master | ", "res : ", res);
+                arvalid <= 0;
+                rpos    <= 0;
+                rready  <= 1;
+            end
+            if (rready & rvalid) begin
+                $display($time, " | master | ", "rcv rdata : ", rdata, " ind : %1d", rpos);
+                inregs[rpos] <= rdata;
+                rpos <= rpos + 1;
+            end
+            if (rready & rvalid & rlast) begin
+                rready  <= 0;
+                arvalid <= 1;
+            end
+
+        end
+    end
+
+
+endmodule
--- src/axi4_stream_slave_wrapper.sv
+++ src/axi4_stream_slave_wrapper.sv
@@ -1,27 +1,142 @@
-module axi4_stream_slave_wrapper #(
-    parameter int SZ = 32
+module axi4_slave_wrapper #(
+    parameter int SZ  = 32,
+    parameter int ASZ = 2,
+    parameter int DSZ = 8
 ) (
     input _rst,
-    input clk
+    input clk,
+    // AW
+    input [ASZ-1:0] awaddr,
+    input awvalid,
+    output reg awready,
+    // W
+    input [DSZ-1:0] wdata,
+    input wvalid,
+    output reg wready,
+    input wlast,
+    // B
+    output reg bresp,  // 1 => ok
+    output reg bvalid,
+    input bready,
+    // AR
+    input [ASZ-1:0] araddr,
+    input arvalid,
+    output reg arready,
+    // R
+    output reg [DSZ-1:0] rdata,
+    output reg rvalid,
+    input rready,
+    output reg rlast,
+    output reg rresp  // 1 => ok
 );
 
-    reg [SZ-1:0] areg;
-    reg [SZ-1:0] breg;
+    reg [DSZ-1:0] inregs[(1<<ASZ) * (SZ/DSZ)];
     reg start;
 
-    wire [2*SZ-1:0] res;
-    wire ready;
+    wire [DSZ-1:0] outregs[2*SZ/DSZ];
+    wire mulready;
 
 
     mult #(
         .SZ(SZ)
     ) main_module (
-        .a(areg),
-        .b(breg),
-        .res(res),
+        .a({inregs[3], inregs[2], inregs[1], inregs[0]}),
+        .b({inregs[7], inregs[6], inregs[5], inregs[4]}),
+        .res({
+            outregs[7],
+            outregs[6],
+            outregs[5],
+            outregs[4],
+            outregs[3],
+            outregs[2],
+            outregs[1],
+            outregs[0]
+        }),
         .start(start),
-        .ready(ready),
+        .ready(mulready),
         ._rst(_rst),
         .clk(clk)
     );
+
+    reg [ASZ-1:0] awaddr_reg;
+    reg [ASZ-1:0] araddr_reg;
+    reg [7:0] wpos;
+    reg [7:0] rpos;
+
+    int i;
+    always @(posedge clk, negedge _rst) begin
+        if (~_rst) begin
+            // zero all regs
+            wpos <= 0;
+            rpos <= 0;
+            awaddr_reg <= 0;
+            araddr_reg <= 0;
+            for (i = 0; i < (1 << ASZ) * (SZ / DSZ); i = i + 1) begin
+                inregs[i] <= 0;
+            end
+            start   <= 1;
+
+            wready  <= 0;
+            bresp   <= 0;
+            bvalid  <= 0;
+            rdata   <= 0;
+            rvalid  <= 0;
+            rlast   <= 0;
+            rresp   <= 0;
+
+
+            awready <= 1;  // accept write req
+            arready <= 1;  // accept read req
+        end
+        else begin
+            if (awready & awvalid) begin
+                awaddr_reg <= awaddr;
+                wpos <= awaddr * (SZ / DSZ);
+                awready <= 0;  // end addr read
+                wready <= 1;  // begin data write
+            end
+            if (wready & wvalid) begin  // writing
+                $display($time, " | slave | ", "rcv wdata : ", wdata, " ind : %1d", wpos);
+                inregs[wpos] <= wdata;  // write to selected byte of input
+                wpos <= wpos + 1;
+            end
+            if (wready & wvalid & wlast) begin
+                wready <= 0;  // end data write
+                bresp  <= 1;  // begin write resp
+                bvalid <= 1;
+            end
+            if (bready & bvalid) begin
+                bvalid  <= 0;  // end write resp
+                awready <= 1;  // begin addr read
+            end
+
+
+            if (arready & arvalid & mulready) begin
+                $display($time, " | slave | ", "snd rdata : ", outregs[0], " ind : %1d", 0);
+                araddr_reg <= araddr;
+                arready <= 0;  // end addr read
+                rdata <= outregs[0];  // begin data read
+                rpos <= 0 + 1;
+                rvalid <= 1;
+                rresp <= 1;
+                rlast <= 0;
+            end
+            if (rready & rvalid) begin
+                $display($time, " | slave | ", "snd rdata : ", outregs[rpos], " ind : %1d", rpos);
+                rdata <= outregs[rpos];
+                rpos  <= rpos + 1;
+                rlast <= rpos == (2 * SZ / DSZ - 1);
+            end
+            if (rready & rvalid & rlast) begin
+                rvalid  <= 0;  // end data read
+                rdata   <= 0;
+                rlast   <= 0;
+                rresp   <= 0;
+                arready <= 1;  // begin addr read
+            end
+
+        end
+    end
+
+
 endmodule
